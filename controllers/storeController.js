aa8e6c7e685773cab135918706b446fa04df8b76
Carlos Rangel <charlesleimaginant@gmail.com>
2022-02-08 13:56:12 -0600

    Create Store Model, and Using Mixins to saving Stores

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,8 +1,15 @@
-exports.myMiddleware = (req, res, next) => {
-    req.name = 'Wes';
-    next();
-}
 exports.homePage = (req, res) => {
     console.log(req.name);
     res.render('index');
+};
+
+
+exports.addStore = (req, res) => {
+    res.render('editStore', { title: 'Add Store'});
+};
+
+
+exports.createStore = (req, res) => {
+    console.log(req.body);
+    res.json(req.body);
 };
