b337a772722e0221394f6ed2a55eabb25410756f
Carlos Rangel <charlesleimaginant@gmail.com>
2022-02-08 17:39:56 -0600

    Add async await to send data to MongoDB

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
     console.log(req.name);
     res.render('index');
@@ -9,7 +12,9 @@ exports.addStore = (req, res) => {
 };
 
 
-exports.createStore = (req, res) => {
+exports.createStore = async (req, res) => {
     console.log(req.body);
-    res.json(req.body);
+    const store = new Store(req.body);
+    await store.save();       // se mueve a la siguiente línea hata que funciona con await es async
+    res.redirect('/');
 };
