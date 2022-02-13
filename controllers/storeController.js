e2f49a3b07115ace888a970f29bca78f93ff1d57
Carlos Rangel <charlesleimaginant@gmail.com>
2022-02-12 23:37:39 -0600

    Custom MongoDB aggretations and multiple query promises with Async await to show the store by tag.

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -85,5 +85,16 @@ exports.getStoreBySlug = async (req, res, next) => {
     const store = await Store.findOne({ slug: req.params.slug });
     if (!store) return next();                                   // Checa si el query a mongo regresa null
     res.render('store', { store, title: store.name });
-  };
+};
+
+exports.getStoresByTag = async (req, res) => {
+    const tag = req.params.tag;
+    const tagQuery = tag || { $exists: true, $ne: [] };
   
+    const tagsPromise = Store.getTagsList();
+    const storesPromise = Store.find({ tags: tagQuery });
+    const [tags, stores] = await Promise.all([tagsPromise, storesPromise]);
+  
+  
+    res.render('tag', { tags, title: 'Tags', tag, stores });
+};
