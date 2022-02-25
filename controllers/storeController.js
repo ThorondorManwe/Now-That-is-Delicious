49b798915b1194d9b6afddd021879c98e8ffe0c4
Carlos Rangel <charlesleimaginant@gmail.com>
2022-02-24 22:03:56 -0600

    Add functionalty to lock down the application with user perissions

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -46,6 +46,7 @@ exports.resize = async (req, res, next) => {
 
 
 exports.createStore = async (req, res) => {
+    req.body.author = req.user._id;
     console.log(req.body);
     const store = await (new Store(req.body)).save();
     //await store.save();       // se mueve a la siguiente línea hata que funciona con await es async
@@ -59,10 +60,17 @@ exports.getStores = async (req, res) => {
     res.render('stores', { title: 'Stores', stores });
 };
 
+const confirmOwner = (store, user) => {
+    if (!store.author.equals(user._id)) {
+      throw Error('You must own a store in order to edit it!');
+    }
+};
+
 exports.editStore = async (req, res) => {
     // 1. Find the store given the ID
     const store = await Store.findOne({ _id: req.params.id });
     // 2. Confirm they are the owner of the store TODO
+    confirmOwner(store, req.user);
     // 3. Render out the edit form so the user can update their store
     res.render('editStore', { title: `Edit ${store.name}`, store});
 }
@@ -82,7 +90,7 @@ exports.updateStore = async (req, res) => {
 
 
 exports.getStoreBySlug = async (req, res, next) => {
-    const store = await Store.findOne({ slug: req.params.slug });
+    const store = await Store.findOne({ slug: req.params.slug }).populate('author'); 
     if (!store) return next();                                   // Checa si el query a mongo regresa null
     res.render('store', { store, title: store.name });
 };
